becc7cd8d8f6e186cdca50887c4beb1fd7701063
jshardy13 <jordan.s.hardy@gmail.com>
2021-10-21 18:12:45 +1000

    added userlist component to map through users array and print User li elements to the DOM

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import AddUser from "./Users/AddUser";
+import UsersList from "./Users/UsersList";
 
 function App() {
    return (
       <div className="App">
          <h1>App started</h1>
          <AddUser />
+         <UsersList />
       </div>
    );
 }
